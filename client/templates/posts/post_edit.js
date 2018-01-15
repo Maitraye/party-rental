b9d94586a57b65d4add6cbe8d62e94a00d54a778
Maitraye <maitraye.urmi@gmail.com>
2018-01-15 04:51:46 -0500

    restrict duplicate url pasting by post edit: exercise

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -6,14 +6,42 @@ Template.postEdit.events({
 			url: $(e.target).find('[name=url]').val(),
 			title: $(e.target).find('[name=title]').val()
 		}
-		Posts.update(currentPostId, {$set: postProperties}, function(error) {
+		// Here the post is updated directly, 
+		// but to check for duplicate links it should be updated through a Meteor method call,
+		
+		Meteor.call('postEdit', postProperties, function(error, result) {
+			if (error) {	
+				// display the error to the user
+				alert(error.reason);
+			} else {
+				// if the url already exists, result_id will be different from currentPostId. Route to the resultId
+				if (result._id != currentPostId) {
+					alert('This link has already been posted');
+					Router.go('postPage', {_id: result._id});
+				} 
+				// if the url does not exist, result_id will be same with currentPostId. Update and route to currentPostId
+				else {
+					Posts.update(currentPostId, {$set: postProperties}, function(error) {
+						if (error) {	
+							// display the error to the user
+							alert(error.reason);
+						} else {
+							Router.go('postPage', {_id: currentPostId});
+						}
+					});
+				}
+			}
+
+		}); 
+
+		/*Posts.update(currentPostId, {$set: postProperties}, function(error) {
 			if (error) {	
 				// display the error to the user
 				alert(error.reason);
 			} else {
 				Router.go('postPage', {_id: currentPostId});
 			}
-		});
+		}); */
 	},
 
 	'click .delete': function(e) {
