a47834f559270a392d9a0766fc8ec9d9c72e859b
Maitraye <maitraye.urmi@gmail.com>
2018-01-15 13:08:43 -0500

    post description shows with post

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,26 +1,3 @@
-/*var postsData = [
-	{
-		title: 'Introducing Telescope',
-		url: 'http://sachagreif.com/introducing-telescope/'
-	},
-	{
-		title: 'Meteor',
-		url: 'http://meteor.com'
-	},
-	{
-		title: 'The Meteor Book',
-		url: 'http://themeteorbook.com'
-	}
-];
-Template.postsList.helpers({
-	posts: postsData
-});*/
-
-/*Template.postsList.helpers({
-	posts: function() {
-		return Posts.find();
-	}
-});*/
 
 // sort post list by timestamps of submitted post
 Template.postsList.helpers({
@@ -29,9 +6,3 @@ Template.postsList.helpers({
 	}
 });
 
-/* show posts by bob-smith with category JavaScript
-Template.posts.helpers({
-	posts: function(){
-		return Posts.find({author: 'bob-smith', category: 'JavaScript'});
-	}
-}); */
